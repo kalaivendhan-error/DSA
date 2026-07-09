fd5d87dfa94b32c81c8b4e7343d528fc4ad37ef5
kalaivedhan-error <kalaivendan55555@gmail.com>
2026-07-09 10:18:26 +0530

    Optimization of circular queue in Array

--- circularQueue.js
+++ circularQueue.js
@@ -1,42 +1,50 @@
 class Cqueue{
-    constructor(size){
+    constructor(capacity){
         this.items=[]
         this.front=0
         this.rear=0
-        this.size=size
+        this.size=0
+        this.capacity=capacity
     }
 
     enqueue(value){
-        if(this.items.length==this.size){
-            console.log('Queue overflow !!!, Queue is already full')
+        if(this.isFull()){
+            console.log('Queue is Full !!!')
             return null;
         }
-        
         this.items[this.rear]=value
-        this.rear++
-        console.log(`${value} added to Queue`)
+        this.rear=(this.rear +1) % this.capacity 
+        this.size++
     }
 
     dequeue(){
-        if(this.front==this.rear){
-            console.log("Failed !!!, Queue is Empty")
+        if(this.isEmpty()){
+            console.log('Failed!!!!, Queue is empyt')
             return null;
         }
-        console.log(`${this.items[0]} is removed`)
-        this.items.splice(0,1)
-        this.rear--
+        this.items[this.front]=null
+        this.front= (this.front+1) % this.capacity
+        this.size--
     }
 
     isEmpty(){
-        return this.front==this.rear
+        return this.size ==0
     }
 
-    length(){
-        return this.rear
+    isFull(){
+        return this.capacity==this.size
     }
 
     first(){
         return this.items[this.front]
+    }
+
+    display(){
+        let result=[]
+        if(this.rear>this.front){
+            return result=this.items.slice(this.front, this.rear)
+        }else{
+        return result=this.items.slice(this.front,this.capacity).concat(this.items.slice(0,this.rear))}
     }
 
 }
@@ -51,11 +59,15 @@ cqueue.enqueue(40)
 cqueue.enqueue(50)
 
 cqueue.dequeue()
+cqueue.dequeue()
 
-cqueue.enqueue(60)
-console.log(cqueue.length())
-console.log(cqueue.isEmpty())
 console.log(cqueue.first())
 
+cqueue.enqueue(60)
+cqueue.enqueue(70)
+cqueue.enqueue(80)
+console.log(cqueue.display())
 
-console.log(cqueue)
+
+
+// console.log(cqueue)

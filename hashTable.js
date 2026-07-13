7bf8d4c3099dc36ae86063692d3d3f95ec17a8ed
kalaivedhan-error <kalaivendan55555@gmail.com>
2026-07-13 22:12:21 +0530

    Solving problem of Hash Table collision

--- hashTable.js
+++ hashTable.js
@@ -6,25 +6,50 @@ class Hashtable{
     hash(key){
         let total=0
         for(let i=0; i<key.length; i++){
-            total+=key.charCodeAt(i)
-        }
-        // console.log(total% this.size)
+            total+=key.charCodeAt(i)}
         return total % this.size
     }
 
     set(key,value){
         const index=this.hash(key)
-        this.table[index]=value
+        // this.table[index]=value
+        var bucket=this.table[index]
+        if(!bucket){
+            this.table[index]=[[key, value]]
+        }else{
+            let sameKey=bucket.find(item => item[0]==key)
+            if(sameKey){
+                sameKey[1]=value
+            }else{
+                bucket.push([key, value])
+            }
+        }
     }
 
     get(key){
         const index= this.hash(key)
-        return this.table[index]
+        // return this.table[index]
+        const bucket=this.table[index]
+        if(bucket){
+            const targetKey=bucket.find(item =>item[0]===key)
+            if(targetKey){
+                return targetKey[1]
+            }
+        }else{
+            return undefined
+        }
     }
 
     remove(key){
         const index=this.hash(key)
-        this.table[index]=undefined
+        // this.table[index]=undefined
+        const bucket=this.table[index]
+        if(bucket){
+            var targetKey=bucket.find(item => item[0]==key)
+            if(targetKey){
+                bucket.splice(bucket.indexOf(targetKey),1)
+            }
+        }
     }
 
     print(){
@@ -39,10 +64,15 @@ class Hashtable{
 const table=new Hashtable(50)
 // table.hash('kalai')
 table.set('name','kalai')
-table.set('frd','Miss d')
+table.set('nickname','error')
+table.set('mane','Miss d')
 table.set('gf','Nisha')
+table.set('fg','Monisha')
 table.set('age',25)
-table.remove('frd')
-console.log(table.get('name'))
+table.remove('name')
+table.remove('fg')
+table.remove('age')
+table.set('age',20)
 table.print()
-console.log(table.table)
+// console.log(table.get('name'))
+// console.log(table.table)
